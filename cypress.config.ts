2a1b1a966538ba4c900268fa86c27cfd8b36d4e5
julianaGiraldo <julys1702@gmail.com>
2023-09-27 17:34:15 -0500

    ejecutores de integracion continua

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,8 +5,8 @@ export default defineConfig({
     // the e2e or component configuration
     e2e: {
         setupNodeEvents(on, config) {
-            // modify config values examples
-            // config.defaultCommandTimeout = 10000
+            config.defaultCommandTimeout = 20000
+            config.responseTimeout = 20000
 
             // IMPORTANT return the updated config object
             return config;
